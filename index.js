82c9b957adbaf7b4a201f242077c4b5004749a5f
courtneywkwong <cwkwong95@gmail.com>
2019-08-29 10:46:53 -0400

    modularizing server/index before npm
    Co-authored-by: courtneywkwong <cwkwong95@gmail.com>
    Co-authored-by: andywongdev <andywong.ny@gmail.com>
    Co-authored-by: jackiel12 <jackie.lin128@gmail.com>
    Co-authored-by: onaumova <leliknaum@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 // This is our entry file for npm
+const WebSocket = require("ws");
+//our library:
+    //exports entire fxanlity to intercept andy's app;
+//andy would invoke this, pass in his app;
+module.exports = (app) => {
+    //create new ws instance
+    const newWS = new WebSocket(app);
+    app.use('*', (req, res, next) => {
+        
+    })
+    //do app.use to caputure all incoming/outgoing http reqs from andy's app;
+        //fetch our /dev-tools to spin up our gui
+    // app.get(‘/lightning-comm’, (req,res)=> res.sendFile(‘path/to/gui/html’));
+    //reassign the res,send fx;
+
+}
 
 // TODO: Refactor server.js (Line 40) to route all user requests through this middleware
+
--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ wss.on("connection", function connection(ws) {
   app.use(cookieParser());
   //Listen on all Requests
   app.use("*", (req, res, next) => {
-
     //Data we're going to send to the establish WebSocket Server for the Front-end to grab;
     //All set as null until we define the data coming in.
     const reqData = {
@@ -24,13 +23,11 @@ wss.on("connection", function connection(ws) {
       body: null,
       type: null
     };
-    
     //Defining any requests that comes in such as Methods, Headers, Cookies & Body.
     !req.method ? reqData.type = 'Method not sent' : reqData.type = req.method;
     !req.headers ? reqData.header = 'Headers not sent' : reqData.header = req.headers;
     !req.cookies ? reqData.cookies = 'There are no cookies' : reqData.cookies = req.cookies;
     !req.body ? reqData.body = 'There is no body' : reqData.body = req.body;
-
     //Sending data object to WebSocket Server for Front-End to grab.
     ws.send(JSON.stringify(reqData));
     //Connection Confirmation.
@@ -38,26 +35,21 @@ wss.on("connection", function connection(ws) {
     next();
   });
   //[INSERT YOUR ROUTES HERE];
-  app.get('/api/test', (req, res) => {
-    fetch('https://pokeapi.co/api/v2/pokemon/bibarel')
-      .then(data => data.json())
-      .then(data => {
-        const resData = {
-          header: null,
-          cookies: null,
-          body: null,
-          type: null
-        };
-        //Defining any response that comes in such as Methods, Headers, Cookies & Body.
-        !data.method ? resData.type = 'Method not sent' : resData.type = data.method;
-        !data.headers ? resData.header = 'Headers not sent' : resData.header = data.headers;
-        !data.cookies ? resData.cookies = 'There are no cookies' : resData.cookies = data.cookies;
-        !data.body ? resData.body = 'There is no body' : resData.body = data.body;
-        console.log('RESPONSE DATA', data)
-        
-        res.status(200).send(data)
-        ws.send(JSON.stringify(data));
-      })
+  app.get('/dev-tools', (req, res) => {
+    const reqData = {
+      header: null,
+      cookies: null,
+      body: null,
+      type: null
+    };
+    //Defining any requests that comes in such as Methods, Headers, Cookies & Body.
+    !req.method ? reqData.type = 'Method not sent' : reqData.type = req.method;
+    !req.headers ? reqData.header = 'Headers not sent' : reqData.header = req.headers;
+    !req.cookies ? reqData.cookies = 'There are no cookies' : reqData.cookies = req.cookies;
+    !req.body ? reqData.body = 'There is no body' : reqData.body = req.body;
+    //Sending data object to WebSocket Server for Front-End to grab.
+    ws.send(JSON.stringify(reqData));
+    res.status(200).send(reqData)
     })
 });
 
